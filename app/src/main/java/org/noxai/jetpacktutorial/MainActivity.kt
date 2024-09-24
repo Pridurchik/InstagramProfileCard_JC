ebc0de1809c8e005df40ae2a65df8add8168914f
Pridurchik <magomedovgarun02@gmail.com>
2024-09-24 19:37:36 +0300

    Таблица умножения

--- app/src/main/java/org/noxai/jetpacktutorial/MainActivity.kt
+++ app/src/main/java/org/noxai/jetpacktutorial/MainActivity.kt
@@ -3,64 +3,39 @@ package org.noxai.jetpacktutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import org.noxai.jetpacktutorial.ui.theme.JetpackTutorialTheme
+import org.noxai.jetpacktutorial.ui.theme.composable.InstagramProfileCard
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TimesTable()
-        }
-    }
-
-
-}
-@Preview(showBackground = true)
-@Composable
-fun TimesTable() {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-    ) {
-        for (i in 1 until 10) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-
-            ){
-                for(j in 1 until 10) {
-
-                    val color : Color = if((j + i) % 2 == 0) {
-                        Color.Yellow
-                    } else {
-                        Color.White
-                    }
-
-                    Box(
-                        modifier = Modifier.fillMaxSize()
-                            .weight(1f)
-                            .border(1.dp, color = Color.Black)
-                            .background(color = color),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(text = (j * i).toString())
-                    }
-                }
+            JetpackTutorialTheme {
+                InstagramProfileCard()
             }
         }
     }
+
 }
+
+
+
